97df032a9c1baaadaae44dd8bc39610f28f1982b
Dan Vega <danvega@gmail.com>
2021-07-27 14:16:56 -0400

    making the books array list modifiable

---
+++ README.md
@@ -0,0 +1,2 @@
+# Building REST API's in Spring Boot
+
--- src/main/java/dev/danvega/restapidemo/controller/BookController.java
+++ src/main/java/dev/danvega/restapidemo/controller/BookController.java
@@ -3,20 +3,21 @@ package dev.danvega.restapidemo.controller;
 import dev.danvega.restapidemo.model.Book;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
-    private List<Book> books;
+    private List<Book> books = new ArrayList<>();
 
     public BookController() {
-        books = List.of(
-                new Book(1,"Hacking with Spring Boot 2.3: Reactive Edition", "Greg L. Turnquist", "Amazon"),
-                new Book(2, "Spring Boot: Up and Running", "Mark Heckler", "OReilly Media, Inc")
-                );
+        books.addAll(Arrays.asList(
+            new Book(1,"Hacking with Spring Boot 2.3: Reactive Edition", "Greg L. Turnquist", "Amazon"),
+            new Book(2, "Spring Boot: Up and Running", "Mark Heckler", "OReilly Media, Inc")
+        ));
     }
 
     @GetMapping
@@ -24,27 +25,25 @@ public class BookController {
         return books;
     }
 
-    @GetMapping("/{id}") // api/books/1
+    @GetMapping("/{id}")
     public Book findById(@PathVariable Integer id) {
        return books.stream().filter(book -> book.getId() == id).findFirst().get();
     }
 
     @PostMapping
-    public void create(@Valid Book book) {
-        // before we event get here
+    public void create(Book book) {
+
     }
 
     @PutMapping("/{id}")
-    public void update(@Valid Book book) {
-        // update an existing book
+    public void update(Book book) {
+
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
-        // delete by id
-    }
 
-    // more endpoints
+    }
 
     public void findBookByAuthor() {
 
